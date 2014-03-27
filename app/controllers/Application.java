c5d4f3c8a2c7485abcf69c4974e5ae440d6c2d01
MattRamsay <matt@youspeakgerman.com>
2014-03-27 22:54:34 +0000

    Sorted out navbar, added widget and made a start on making markup dynamic/json data-driven

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,6 +2,11 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
+import play.libs.WS;
+import play.mvc.Result;
+
+import static play.libs.F.Function;
+import static play.libs.F.Promise;
 
 import views.html.*;
 
@@ -10,5 +15,16 @@ public class Application extends Controller {
     public static Result index() {
         return ok(views.html.index.render("Your place for a new home"));
     }
-
+	
+	public static Promise<Result> getHomes(String postcode) {
+		String feedUrl = "http://slickindustries.co.uk/homes/" + postcode;
+		final Promise<Result> resultPromise = WS.url(feedUrl).get().map(
+				new Function<WS.Response, Result>() {
+					public Result apply(WS.Response response) {
+						return ok(response.asJson());
+					}
+				}
+		);
+		return resultPromise;
+	}
 }
